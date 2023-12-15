7c5b3e4836626c37e5a9b32627e6e4f643716f34
nel2222 <114640151+nel2222@users.noreply.github.com>
2023-12-15 13:07:22 +0100

    jh

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
   content: ["./main/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        "color-primary": "#fc0",
-        "color-red": "#d40511",
-        "color-gray": "#f2f2f2",
-      },
       fontFamily: {
         first: ["Nunito Sans"],
       },
